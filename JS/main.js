070546cb243422a72f6ed9ca1473840f3fe81529
Reezurect <zudilin2014@yandex.ru>
2023-06-20 21:00:34 +0300

    add: JS

--- JS/main.js
+++ JS/main.js
@@ -2,16 +2,33 @@ const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
 const mMenuToggel = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
-window.addEventListener('scroll', () => {
-  if (this.scrollY > 1) {
+
+const lightModeOn = (event) => {
     navbar.classList.add("navbar-light");
     logo.href.baseVal = "img/sprite.svg#logo";
-  } else {
+};
+const lightModeOff = (event) => {
     navbar.classList.remove("navbar-light");
-    logo.href.baseVal = "img/sprite.svg#logo-light"
-  }
+    logo.href.baseVal = "img/sprite.svg#logo-light";
+};
+
+const openMenu = (event) => { // функция открывания меню
+    menu.classList.add("is-open"); // вешает класс is-open
+    mMenuToggel.classList.add("close-menu");
+    document.body.style.overflow = "hidden"; // запрещает прокрутку сайта под иеню
+    lightModeOn();
+};
+const closeMenu = (event) => { // функция закрывания меню
+    menu.classList.remove("is-open"); // убирает класс is-open
+    mMenuToggel.classList.remove("close-menu");
+    document.body.style.overflow = ""; // возвращает прокрутку сайта под иеню
+    lightModeOff();
+};
+
+window.addEventListener("scroll", () => {
+  this.scrollY > 1 ? lightModeOn() : lightModeOff(); 
 });
 mMenuToggel.addEventListener("click", (event) => {
-event.preventDefault();
-menu.classList.toggle("is-open");
+  event.preventDefault();
+  menu.classList.contains("is-open") ? closeMenu() : openMenu();
 });
