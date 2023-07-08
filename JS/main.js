cbdbbe3fd6e1900a3d1a4a80f90de8feef138e39
Reezurect <zudilin2014@yandex.ru>
2023-07-08 21:38:41 +0300

    add: section blod

--- JS/main.js
+++ JS/main.js
@@ -85,3 +85,18 @@ const swiper = new Swiper('.features-slider', {
     },
   },
 });
+
+const swiperBlog = new Swiper(".blog-slider", {
+  speed: 400,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.blog-button-next',
+    prevEl: '.blog-button-prev',
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+  },
+});
